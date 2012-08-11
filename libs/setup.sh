f642a1ab1a566b86febcd476f55ca5482e2148d0
Takashi Masuyama <mamewotoko@gmail.com>
2012-08-11 12:05:25 +0900

    download jars as specified name

--- libs/setup.sh
+++ libs/setup.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 URL=http://robotium.googlecode.com/files/robotium-solo-3.2.1.jar
-wget $URL
+wget $URL -O robotium-solo-3.2.1.jar
 URL2=http://scirocco.googlecode.com/files/scirocco_v2.0.jar
-wget $URL2
+wget $URL2 -O scirocco_v2.0.jar
